769656f060c1a4e4412efa0e74d80ff866d49139
Brian Choi <choibrian97@gmail.com>
2021-02-01 02:22:53 -0800

    Change spacing to match atom settings

--- Module 1 - Manipulating Images/convert_images.py
+++ Module 1 - Manipulating Images/convert_images.py
@@ -4,9 +4,9 @@ from PIL import Image
 import os
 
 for file in os.listdir('images'):
-  if file == '.DS_Store':
-    continue
-  image = 'images/' + file
-  im = Image.open(image)
-  new_im = im.rotate(270).resize((128,128)).convert('RGB')
-  new_im.save('opt/icons/' + file, 'JPEG')
+    if file == '.DS_Store':
+       continue
+    image = 'images/' + file
+    im = Image.open(image)
+    new_im = im.rotate(270).resize((128,128)).convert('RGB')
+    new_im.save('opt/icons/' + file, 'JPEG')
