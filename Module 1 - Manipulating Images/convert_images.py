eff747fb65ea845b7e713a0bf9a14e8c351084d7
Brian Choi <choibrian97@gmail.com>
2021-02-01 02:21:33 -0800

    Add submitted code

---
+++ Module 1 - Manipulating Images/convert_images.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+for file in os.listdir('images'):
+  if file == '.DS_Store':
+    continue
+  image = 'images/' + file
+  im = Image.open(image)
+  new_im = im.rotate(270).resize((128,128)).convert('RGB')
+  new_im.save('opt/icons/' + file, 'JPEG')
