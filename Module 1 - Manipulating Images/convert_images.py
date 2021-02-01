9d6902b8b5d5b240553c5a5139ca54b84a7ce68b
Brian Choi <choibrian97@gmail.com>
2021-02-01 02:26:17 -0800

    Add comment for .DS_Store exclusion

--- Module 1 - Manipulating Images/convert_images.py
+++ Module 1 - Manipulating Images/convert_images.py
@@ -4,8 +4,10 @@ from PIL import Image
 import os
 
 for file in os.listdir('images'):
+    # This file is not an image
     if file == '.DS_Store':
        continue
+    
     image = 'images/' + file
     im = Image.open(image)
     new_im = im.rotate(270).resize((128,128)).convert('RGB')
