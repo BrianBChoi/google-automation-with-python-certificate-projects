0bc4f84c4566b58d67a7dd697f16e23068835de2
Brian Choi <choibrian97@gmail.com>
2021-02-02 00:45:56 -0800

    Change method names to follow naming conventions

--- Module 2 - Processing and Uploading Text Files/run.py
+++ Module 2 - Processing and Uploading Text Files/run.py
@@ -3,16 +3,16 @@
 import os
 import requests
 
-def processFile(file):
+def process_file(file):
     review = {}
     # open file and process review
     return review
 
-def uploadFile(review):
+def upload_file(review):
     response = requests.post('http://<REPLACE WITH IP>/feedback', json=review)
     response.raise_for_status()
 
 if __name__ == '__main__':
     for files in os.listdir('data/feedback'):
-        review = processFile(file)
-        uploadFile(review)
+        review = process_file(file)
+        upload_file(review)
