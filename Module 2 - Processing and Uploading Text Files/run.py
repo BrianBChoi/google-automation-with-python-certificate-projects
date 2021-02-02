5c7fd9b929c396ce693c6116e112b4757da84b05
Brian Choi <choibrian97@gmail.com>
2021-02-02 00:33:45 -0800

    Start processFile() and uploadFile()

--- Module 2 - Processing and Uploading Text Files/run.py
+++ Module 2 - Processing and Uploading Text Files/run.py
@@ -3,6 +3,14 @@
 import os
 import requests
 
+def processFile(file):
+    review = {}
+    # open file and process review
+    return review
+
+def uploadFile(review):
+    # use requests to make an HTTP POST request with the review dictionary
+
 if __name__ == '__main__':
     for files in os.listdir('data/feedback'):
         review = processFile(file)
