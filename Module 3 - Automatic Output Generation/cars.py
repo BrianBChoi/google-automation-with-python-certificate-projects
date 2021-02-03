7c7cf1e6551cc865d2bc5519fd51aeae6ef89c54
Brian Choi <choi.brian.97@gmail.com>
2021-02-03 12:55:28 -0800

    Add car with max sales to summary

--- Module 3 - Automatic Output Generation/cars.py
+++ Module 3 - Automatic Output Generation/cars.py
@@ -40,6 +40,7 @@ def process_data(data):
   summary = [
     "The {} generated the most revenue: ${}".format(
       format_car(max_revenue["car"]), max_revenue["revenue"]),
+    "The {} had the most sales: {}".format(format_car(max_sales["car"]), max_sales["sales"])
   ]
 
   return summary
