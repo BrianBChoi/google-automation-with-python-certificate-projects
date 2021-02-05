a011ac1b8fb4f2818422df8c2828cd2bc01e29db
Brian Choi <choi.brian.97@gmail.com>
2021-02-05 00:08:34 -0800

    Paste given code to generate an email

--- Module 4 - Online Fruit Shop/email_report.py
+++ Module 4 - Online Fruit Shop/email_report.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
+import email.message
+import mimetypes
+import os.path
+import smtplib
+
+def generate(sender, recipient, subject, body, attachment_path):
+  """Creates an email with an attachement."""
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recipient
+  message["Subject"] = subject
+  message.set_content(body)
+  attachment_filename = os.path.basename(attachment_path)
+  mime_type, _ = mimetypes.guess_type(attachment_path)
+  mime_type, mime_subtype = mime_type.split('/', 1)
+
+  with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(),
+                          maintype=mime_type,
+                          subtype=mime_subtype,
+                          filename=attachment_filename)
+
+  return message
 
 if __name__ == "__main__":
   # generate email
-  # send email 
+  sender = "automation@example.com"
+  recipient = "<REPLACE_WITH_USERNAME>@example.com"
+  subject = "Upload Completed - Online Fruit Store"
+  body = "All fruits are uploaded to our website successfully. "\
+         "A detailed list is attached to this email."
+  attachment = "/tmp/processed.pdf"
+  generate(sender, recipient, subject, body, attachment)
+
+  # send email
