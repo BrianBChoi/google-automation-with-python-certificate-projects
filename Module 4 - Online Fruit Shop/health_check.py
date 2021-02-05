1623350f97194e5e07855a1d417b5adcf123d2c9
Brian Choi <choi.brian.97@gmail.com>
2021-02-05 02:14:44 -0800

    Initial plan for health_check.py

--- Module 4 - Online Fruit Shop/health_check.py
+++ Module 4 - Online Fruit Shop/health_check.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+
+if __name__ == "__main__":
+  # check if CPU usage is over 80%
+  # check if available disk space is lower than 20%
+  # check if available memory is less than 500MB
+  # check if hostname "localhost" cannot be resolved to "127.0.0.1"
+  # if a check fails, email a report
