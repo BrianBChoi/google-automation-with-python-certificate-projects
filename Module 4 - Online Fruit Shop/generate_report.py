993ba45f4ca88f494436e626020732e8bfff7ec7
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 23:09:19 -0800

    Implement getting fruit descriptions

--- Module 4 - Online Fruit Shop/generate_report.py
+++ Module 4 - Online Fruit Shop/generate_report.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 
+import os
+from upload_fruit_text import process_files
+
+
 def main(argv):
   # use process_files from upload_fruit_text.py to get all fruit descriptions
+  directory = argv[1]
+  files = os.listdir(directory)
+  os.chdir(directory)
+  descriptions = process_files(files)
+  
   # create report body text with fruit descriptions
   # get date
   # generate report and save
