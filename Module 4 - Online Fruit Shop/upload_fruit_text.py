f0ba3e3e1b3d29ce22bb20f12e7e31256388904d
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 22:12:55 -0800

    Initial plan

--- Module 4 - Online Fruit Shop/upload_fruit_text.py
+++ Module 4 - Online Fruit Shop/upload_fruit_text.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+
+def process_files(directory):
+  """Given a directory, take each text file and add their contents to
+  a dictionary in a list. Return the list of dictionaries.
+  """
+
+
+def upload_descriptions(descriptions):
+  """Given a list of dictionaries, upload it as a JSON object with a
+  POST request.
+  """
+
+
+def main(argv):
+  # Process each text file and add to a list of dictionaries
+  descriptions = process_files(directory)
+  # Upload list of dictionaries as a JSON
+  upload_descriptions(descriptions)
+
+if __name__ == "__main__":
+  main(sys.argv)
