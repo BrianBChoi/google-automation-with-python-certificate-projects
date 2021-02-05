0536127796caee6df8bebf548515d15a2f56c169
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 22:42:06 -0800

    Implement processing text descriptions

--- Module 4 - Online Fruit Shop/upload_fruit_text.py
+++ Module 4 - Online Fruit Shop/upload_fruit_text.py
@@ -9,7 +9,21 @@ def process_files(files):
   """Given a list of files, take each text file and add their contents to
   a dictionary in a list. Return the list of dictionaries.
   """
+  descriptions = []
+  for file in files:
+    with open(file) as text:
+      description = {}
+      for positon, line in enumerate(text):
+        if position == 0:
+          description["name"] = line
+        if position == 1:
+          description["weight"] = line.strip(" lbs")
+        if position == 2:
+          description["description"] = line
+    description["image_name"] = file.strip(".txt") + ".jpeg"
+    descriptions.append(description)
 
+  return descriptions
 
 def upload_descriptions(descriptions):
   """Given a list of dictionaries, upload it as a JSON object with a
@@ -23,6 +37,7 @@ def upload_descriptions(descriptions):
 def main(argv):
   directory = argv[1]
   files = os.listdir(directory)
+  os.chdir(directory)
   descriptions = process_files(files)
   upload_descriptions(descriptions)
 
