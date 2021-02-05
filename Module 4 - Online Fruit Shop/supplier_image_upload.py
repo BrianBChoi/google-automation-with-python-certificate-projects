2463aab75e237a6d7df3cb3de78474ecbbc76fa5
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 20:59:08 -0800

    Implement uploading images

--- Module 4 - Online Fruit Shop/supplier_image_upload.py
+++ Module 4 - Online Fruit Shop/supplier_image_upload.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 import requests
 
 
 def main(argv):
-  # open each image file
-  # upload each file with a POST request
+  image_dir = argv[1]
+  files = os.listdir(image_dir)
+  os.chdir(image_dir)
+
+  for image in files:
+    with open(image, 'rb') as opened:
+      r = requests.post('http://localhost/upload/', files={'file': opened})
+
 
 if __name__ == "__main__":
   main(sys.argv)
