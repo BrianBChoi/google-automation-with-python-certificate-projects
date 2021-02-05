9fdea74657539173c4753f12c16abfde798ebb8d
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 23:06:28 -0800

    Initial plan

--- Module 4 - Online Fruit Shop/generate_report.py
+++ Module 4 - Online Fruit Shop/generate_report.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+
+def main(argv):
+  # use process_files from upload_fruit_text.py to get all fruit descriptions
+  # create report body text with fruit descriptions
+  # get date
+  # generate report and save
+
+if __name__ == "__main__":
+  main(sys.argv)
