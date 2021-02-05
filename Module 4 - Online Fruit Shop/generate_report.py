574d530522275982d43c75cfccd8531b34cd46f0
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 23:46:14 -0800

    Implement generating PDF report

--- Module 4 - Online Fruit Shop/generate_report.py
+++ Module 4 - Online Fruit Shop/generate_report.py
@@ -4,6 +4,10 @@
 import os
 from upload_fruit_text import process_files
 from datetime import datetime
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
 
 def report_text(descriptions):
   report = ""
@@ -11,6 +15,15 @@ def report_text(descriptions):
     report += "name: {}<br/>".format(fruit["name"])
     report += "weight: {} lbs<br/><br/>".format(fruit["weight"])
   return report
+
+
+def generate(filename, title, body_text):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(additional_info, styles["BodyText"])
+  empty_line = Spacer(1,20)
+  report.build([report_title, empty_line, report_info, empty_line, report_table])
 
 
 def main(argv):
@@ -25,8 +38,10 @@ def main(argv):
 
   # get date
   date = datetime.today().strftime('%Y-%m-%d')
-  
+  title = "Processed Update on {}".format(date)
+
   # generate report and save
+  generate("/tmp/processed.pdf", title, report)
 
 if __name__ == "__main__":
   main(sys.argv)
