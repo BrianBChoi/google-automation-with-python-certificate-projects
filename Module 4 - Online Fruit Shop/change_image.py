efda5b8459c17e380c7b2cc0e8cc1bdd24877f78
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 20:09:48 -0800

    Add initial plan for change_image

--- Module 4 - Online Fruit Shop/change_image.py
+++ Module 4 - Online Fruit Shop/change_image.py
@@ -3,9 +3,21 @@
 from PIL import Image
 import os
 
+def get_images(directory):
+  """Given a directory of images, returns a list of their names"""
+
+
+def process_images(images):
+  """Given a list of image file names, changes their size to 600x400 and
+  saves each as a JPEG
+  """
+
 
 def main(argv):
-  
+  # get list of files
+  images = get_images(directory)
+  # change the images and save them
+  process_images(images)
 
 
 if __name__ == "__main__":
