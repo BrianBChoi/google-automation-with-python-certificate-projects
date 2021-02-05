1dadf0163f3086a082d87f20accdbe935e37cd9f
Brian Choi <choi.brian.97@gmail.com>
2021-02-04 22:16:52 -0800

    Implement uploading descriptions

--- Module 4 - Online Fruit Shop/upload_fruit_text.py
+++ Module 4 - Online Fruit Shop/upload_fruit_text.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+import requests
+
+
 def process_files(directory):
   """Given a directory, take each text file and add their contents to
   a dictionary in a list. Return the list of dictionaries.
@@ -11,12 +14,14 @@ def upload_descriptions(descriptions):
   """Given a list of dictionaries, upload it as a JSON object with a
   POST request.
   """
+  response = requests.post(
+    "http://<REPLACE_WITH_IP>/fruits/", json=descriptions)
+  response.raise_for_status()
 
 
 def main(argv):
   # Process each text file and add to a list of dictionaries
   descriptions = process_files(directory)
-  # Upload list of dictionaries as a JSON
   upload_descriptions(descriptions)
 
 if __name__ == "__main__":
