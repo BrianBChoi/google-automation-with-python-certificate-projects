3faaca2c86eb1666c13de2a9d5eb45b5c2a202aa
Brian Choi <choi.brian.97@gmail.com>
2021-02-05 03:10:57 -0800

    Fix typo when generating report

--- Module 4 - Online Fruit Shop/generate_report.py
+++ Module 4 - Online Fruit Shop/generate_report.py
@@ -28,7 +28,7 @@ def generate(filename, title, body_text):
   report_title = Paragraph(title, styles["h1"])
   report_info = Paragraph(body_text, styles["BodyText"])
   empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info, empty_line, report_table])
+  report.build([report_title, empty_line, report_info, empty_line])
 
 
 def main(argv):
