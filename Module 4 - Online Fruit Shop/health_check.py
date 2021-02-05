bf7f32bdaf57ae4ad22e9239fe5f2f0f14309505
Brian Choi <choi.brian.97@gmail.com>
2021-02-05 02:39:51 -0800

    Change sending email to be at end of main

--- Module 4 - Online Fruit Shop/health_check.py
+++ Module 4 - Online Fruit Shop/health_check.py
@@ -11,26 +11,27 @@ if __name__ == "__main__":
   sender = "automation@example.com"
   recipient = "<REPLACE_WITH_USERNAME>@example.com"
   body = "Please check your system and resolve the issue as soon as possible."
+  subject = ""
 
   # check if CPU usage is over 80%
   cpu_usage = psutil.cpu_percent(interval=0.1)
   if cpu_usage > 80:
     subject = "Error - CPU usage is over 80%"
-    message = generate_basic(sender, recipient, subject, body)
-    send(message)
 
   # check if available disk space is lower than 20%
   total, used, free = shutil.disk_usage(os.path.expanduser("~"))
   if free/total < 0.2:
     subject = "Error - Available disk space is less than 20%"
-    message = generate_basic(sender, recipient, subject, body)
-    send(message)
 
   # check if available memory is less than 500MB
   mem_mb = psutil.virtual_memory().available / (1024 ** 2)
   if mem_mb < 500:
     subject = "Error - Available memory is less than 500MB"
-    message = generate_basic(sender, recipient, subject, body)
+
 
   # check if hostname "localhost" cannot be resolved to "127.0.0.1"
+
   # if a check fails, email a report
+  if subject != "":
+    message = generate_basic(sender, recipient, subject, body)
+    send(message)
